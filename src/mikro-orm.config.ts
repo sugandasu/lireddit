1418c6dbcebfa501c614354ae3371f11c921eca5
Arif Suganda <arifsuganda.su@gmail.com>
2021-06-30 15:32:01 +0800

    create user migration

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,6 +2,7 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
+import { User } from "./entities/User";
 
 export default {
   host: "db",
@@ -12,7 +13,7 @@ export default {
     path: path.join(__dirname, "./migrations"),
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: "check-kendaraan-api",
   type: "postgresql",
   debug: !__prod__,
