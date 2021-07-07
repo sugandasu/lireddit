25b9914c9dd0b46b1cac20931497b2699d9c1958
Arif Suganda <arifsuganda.su@gmail.com>
2021-07-07 08:14:56 +0800

    fix express session

--- src/types/myContext.ts
+++ src/types/myContext.ts
@@ -1,8 +1,17 @@
 import { Connection, EntityManager, IDatabaseDriver } from "@mikro-orm/core";
 import { Request, Response } from "express";
+import { Session, SessionData } from "express-session";
+
+declare module "express-session" {
+  interface SessionData {
+    userId?: number;
+  }
+}
 
 export type MyContext = {
   em: EntityManager<IDatabaseDriver<Connection>>;
-  req: Request;
+  req: Request & {
+    session: Session & Partial<SessionData>;
+  };
   res: Response;
 };
