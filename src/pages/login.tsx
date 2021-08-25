bc88e8de5995e933a0ab5b473a56b4a3fd963aa1
Arif Suganda <arifsuganda.su@gmail.com>
2021-08-25 14:01:39 +0800

    create login page

---
+++ src/pages/login.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Formik, Form } from "Formik";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+import { Box, Button } from "@chakra-ui/react";
+import { useLoginMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
+
+const Login: React.FC<{}> = ({}) => {
+    const router = useRouter();
+    const [, login] = useLoginMutation();
+    return (
+        <Wrapper variant="regular">
+            <Formik
+                initialValues={{
+                    username: "",
+                    password: "",
+                }}
+                onSubmit={async (values, { setErrors }) => {
+                    const response = await login({ options: values });
+                    if (response.data?.login.errors) {
+                        setErrors(toErrorMap(response.data.login.errors));
+                    } else if (response.data?.login.user) {
+                        router.push("/");
+                    }
+                }}
+            >
+                {({ isSubmitting }) => (
+                    <Form>
+                        <InputField
+                            name="username"
+                            placeholder="Username"
+                            label="Username"
+                        />
+                        <Box mt={4}>
+                            <InputField
+                                name="password"
+                                placeholder="Password"
+                                label="Password"
+                                type="password"
+                            />
+                        </Box>
+                        <Button
+                            mt={4}
+                            type="submit"
+                            isLoading={isSubmitting}
+                            colorScheme="teal"
+                        >
+                            login
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
+        </Wrapper>
+    );
+};
+
+export default Login;
