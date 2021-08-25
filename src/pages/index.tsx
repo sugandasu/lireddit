00ba9f1de1ee1a24140914bc3a0f6f84092df401
Arif Suganda <arifsuganda.su@gmail.com>
2021-08-25 19:32:07 +0800

    use ssr on index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
 import { withUrqlClient } from "next-urql";
 import { NavBar } from "../components/NavBar";
+import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 
-const Index = () => (
-    <>
-        <NavBar />
-        <div>hello world</div>
-    </>
-);
+const Index = () => {
+    const [{ data }] = usePostsQuery();
+    return (
+        <>
+            <NavBar />
+            <div>hello world</div>
+            <br />
+            {!data
+                ? null
+                : data.posts.map((post) => (
+                      <div key={post.id}>{post.title}</div>
+                  ))}
+        </>
+    );
+};
 
-export default withUrqlClient(createUrqlClient)(Index);
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
