f742d413b16113d22a3926a6ad317eaae20e8c12
Arif Suganda <arifsuganda.su@gmail.com>
2021-08-25 15:13:21 +0800

    update user when register

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,68 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { Provider, createClient } from "urql";
+import { Provider, createClient, dedupExchange, fetchExchange } from "urql";
+import theme from "../theme";
+import { AppProps } from "next/app";
+import { Cache, cacheExchange, QueryInput } from "@urql/exchange-graphcache";
+import {
+    LoginMutation,
+    MeDocument,
+    MeQuery,
+    RegisterMutation,
+} from "../generated/graphql";
+
+function beterUpdateQuery<Result, Query>(
+    cache: Cache,
+    qi: QueryInput,
+    result: any,
+    fn: (r: Result, q: Query) => Query
+) {
+    cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 
 const client = createClient({
     url: "http://localhost:4000/graphql",
     fetchOptions: {
         credentials: "include",
     },
+    exchanges: [
+        dedupExchange,
+        cacheExchange({
+            updates: {
+                Mutation: {
+                    login: (_result, args, cache, info) => {
+                        beterUpdateQuery<LoginMutation, MeQuery>(
+                            cache,
+                            { query: MeDocument },
+                            _result,
+                            (result, query) => {
+                                if (result.login.errors) {
+                                    return query;
+                                } else {
+                                    return { me: result.login.user };
+                                }
+                            }
+                        );
+                    },
+                    register: (_result, args, cache, info) => {
+                        beterUpdateQuery<RegisterMutation, MeQuery>(
+                            cache,
+                            { query: MeDocument },
+                            _result,
+                            (result, query) => {
+                                if (result.register.errors) {
+                                    return query;
+                                } else {
+                                    return { me: result.register.user };
+                                }
+                            }
+                        );
+                    },
+                },
+            },
+        }),
+        fetchExchange,
+    ],
 });
-
-import theme from "../theme";
-import { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
