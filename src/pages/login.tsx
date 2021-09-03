fec75d9d5bf412c751daedaf97260094598e76c5
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-03 19:52:14 +0800

    create forgot password link

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,4 @@
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
 import { Form, Formik } from "Formik";
 import { withUrqlClient } from "next-urql";
 import { useRouter } from "next/router";
@@ -8,6 +8,7 @@ import { Wrapper } from "../components/Wrapper";
 import { useLoginMutation } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { toErrorMap } from "../utils/toErrorMap";
+import NextLink from "next/link";
 
 const Login: React.FC<{}> = ({}) => {
     const router = useRouter();
@@ -43,6 +44,11 @@ const Login: React.FC<{}> = ({}) => {
                                 type="password"
                             />
                         </Box>
+                        <Flex>
+                            <NextLink href="/forgot-password">
+                                <Link ml="auto">forgot password</Link>
+                            </NextLink>
+                        </Flex>
                         <Button
                             mt={4}
                             type="submit"
