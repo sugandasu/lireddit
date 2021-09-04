39ad1e5552c615cf86fff4fb0372352cd70dedf6
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-04 18:32:22 +0800

    delete mikro-orm

--- src/migrations/Migration20210627060003.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20210627060003 extends Migration {
-
-  async up(): Promise<void> {
-    this.addSql('create table "post" ("id" serial primary key, "title" varchar(255) not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null);');
-  }
-
-}
