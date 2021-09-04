667df80c7959c8c60613d5afca93eb93fdb872fa
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-04 18:27:18 +0800

    select first raw user object

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -150,7 +150,7 @@ export class UserResolver {
         })
         .returning("*")
         .execute();
-      user = result.raw;
+      user = result.raw[0];
     } catch (err) {
       if (err.code === "23505") {
         return {
