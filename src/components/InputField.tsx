ede8aed43202ddcfcc79b7e7f78928da2f4cedae
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-05 16:46:47 +0800

    choose input or textarea

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -3,6 +3,7 @@ import {
     FormErrorMessage,
     FormLabel,
     Input,
+    Textarea,
 } from "@chakra-ui/react";
 import { InputHTMLAttributes } from "react";
 import { useField } from "Formik";
@@ -10,19 +11,25 @@ import { useField } from "Formik";
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
     name: string;
     label: string;
+    textarea?: boolean;
 };
 
 export const InputField: React.FC<InputFieldProps> = ({
     label,
+    textarea,
     size: _,
     ...props
 }) => {
+    let InputOrTextarea = Input;
+    if (textarea) {
+        InputOrTextarea = Textarea;
+    }
     const [field, { error }] = useField(props);
 
     return (
         <FormControl isInvalid={!!error}>
             <FormLabel htmlFor={field.name}>{label}</FormLabel>
-            <Input {...field} {...props} id={field.name} />
+            <InputOrTextarea {...field} {...props} id={field.name} />
             {!!error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
         </FormControl>
     );
