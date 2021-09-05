44ca788a7a85935054e4ed98af992f7c065576dc
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-05 16:03:15 +0800

    create  post page

---
+++ src/pages/create-post.tsx
@@ -0,0 +1,48 @@
+import { Box, Button } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
+import React from "react";
+import { InputField } from "../components/InputField";
+import { Wrapper } from "../components/Wrapper";
+
+const CreatePost: React.FC<{}> = ({}) => {
+    return (
+        <Wrapper variant="small">
+            <Formik
+                initialValues={{
+                    title: "",
+                    text: "",
+                }}
+                onSubmit={async (values) => {
+                    console.log(values);
+                }}
+            >
+                {({ isSubmitting }) => (
+                    <Form>
+                        <InputField
+                            name="title"
+                            placeholder="title"
+                            label="Title"
+                        />
+                        <Box mt={4}>
+                            <InputField
+                                name="text"
+                                placeholder="text..."
+                                label="Text"
+                            />
+                        </Box>
+                        <Button
+                            mt={4}
+                            type="submit"
+                            isLoading={isSubmitting}
+                            colorScheme="teal"
+                        >
+                            create post
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
+        </Wrapper>
+    );
+};
+
+export default CreatePost;
