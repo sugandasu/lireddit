fe5028266b6957bc42bc7e17fa9d6d3978aef071
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-06 08:55:28 +0800

    add create post link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
+import { Link } from "@chakra-ui/layout";
 import { withUrqlClient } from "next-urql";
-import { NavBar } from "../components/NavBar";
+import React from "react";
+import { Layout } from "../components/Layout";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
+import NextLink from "next/link";
 
 const Index = () => {
     const [{ data }] = usePostsQuery();
     return (
-        <>
-            <NavBar />
+        <Layout>
+            <NextLink href="/create-post">
+                <Link>create post</Link>
+            </NextLink>
             <div>hello world</div>
             <br />
             {!data
@@ -15,7 +20,7 @@ const Index = () => {
                 : data.posts.map((post) => (
                       <div key={post.id}>{post.title}</div>
                   ))}
-        </>
+        </Layout>
     );
 };
 
