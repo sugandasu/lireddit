aa7ec9262e0495ff7a6819ec3716321b85acef1f
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-06 08:55:53 +0800

    create is authenticated

---
+++ src/utils/useIsAuth.ts
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useMeQuery } from "../generated/graphql";
+
+export const useIsAuth = () => {
+    const [{ data, fetching }] = useMeQuery();
+    const router = useRouter();
+    useEffect(() => {
+        if (!fetching && !data?.me) {
+            router.replace("/login?next=" + router.pathname);
+        }
+        console.log(data, fetching);
+    }, [fetching, data, router]);
+};
