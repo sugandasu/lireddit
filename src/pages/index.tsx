c234d8b6ef56dd72f41ced2199226b60278d793c
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-06 10:49:10 +0800

    use stack to show post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { Layout } from "../components/Layout";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import NextLink from "next/link";
+import { Box, Heading, Stack, Text } from "@chakra-ui/react";
 
 const Index = () => {
     const [{ data }] = usePostsQuery({
@@ -20,7 +21,14 @@ const Index = () => {
             {!data ? (
                 <div>loading...</div>
             ) : (
-                data.posts.map((post) => <div key={post.id}>{post.title}</div>)
+                <Stack spacing={8}>
+                    {data.posts.map((post) => (
+                        <Box key={post.id} p={5} shadow="md" borderWidth="1px">
+                            <Heading fontSize="xl">{post.title}</Heading>
+                            <Text mt={4}>{post.text}</Text>
+                        </Box>
+                    ))}
+                </Stack>
             )}
         </Layout>
     );
