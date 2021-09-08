518a4c6f5937f3f8cdc3a7f86e6a331d138f904d
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-08 11:57:45 +0800

    redesign navbar for logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Link } from "@chakra-ui/react";
+import { Box, Button, Flex, Link, Heading } from "@chakra-ui/react";
 import React from "react";
 import NextLink from "next/link";
 import { useLogoutMutation, useMeQuery } from "../generated/graphql";
@@ -28,7 +28,12 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
     } else if (!data?.me) {
     } else {
         body = (
-            <Flex>
+            <Flex align="center">
+                <NextLink href="/create-post">
+                    <Button as={Link} mr={4}>
+                        create post
+                    </Button>
+                </NextLink>
                 <Box mr={2}>{data.me.username}</Box>
                 <Button
                     onClick={() => {
@@ -43,8 +48,15 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
         );
     }
     return (
-        <Flex zIndex={2} position="sticky" top={0} bg="tomato" p={4}>
-            <Box ml={"auto"}>{body}</Box>
+        <Flex zIndex={2} position="sticky" top={0} bg="tan" p={4}>
+            <Flex flex={1} margin="auto" maxW={800} align="center">
+                <NextLink href="/">
+                    <Link>
+                        <Heading>LiReddit</Heading>
+                    </Link>
+                </NextLink>
+                <Box ml={"auto"}>{body}</Box>
+            </Flex>
         </Flex>
     );
 };
