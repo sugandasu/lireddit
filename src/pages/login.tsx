fe03429d1acd54d73e8d0f9a743eb2d86aa3fc62
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-09 09:40:09 +0800

    invalidate cache after authentication

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -11,61 +11,61 @@ import { toErrorMap } from "../utils/toErrorMap";
 import NextLink from "next/link";
 
 const Login: React.FC<{}> = ({}) => {
-    const router = useRouter();
-    const [, login] = useLoginMutation();
-    return (
-        <Wrapper variant="regular">
-            <Formik
-                initialValues={{
-                    usernameOrEmail: "",
-                    password: "",
-                }}
-                onSubmit={async (values, { setErrors }) => {
-                    const response = await login(values);
-                    if (response.data?.login.errors) {
-                        setErrors(toErrorMap(response.data.login.errors));
-                    } else if (response.data?.login.user) {
-                        if (typeof router.query.next === "string") {
-                            router.push(router.query.next);
-                        } else {
-                            router.push("/");
-                        }
-                    }
-                }}
+  const router = useRouter();
+  const [, login] = useLoginMutation();
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{
+          usernameOrEmail: "",
+          password: "",
+        }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await login(values);
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login.user) {
+            if (typeof router.query.next === "string") {
+              router.push(router.query.next);
+            } else {
+              router.push("/");
+            }
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="usernameOrEmail"
+              placeholder="username or email"
+              label="Username Or Email"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="Password"
+                label="password"
+                type="password"
+              />
+            </Box>
+            <Flex>
+              <NextLink href="/forgot-password">
+                <Link ml="auto">forgot password</Link>
+              </NextLink>
+            </Flex>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="teal"
             >
-                {({ isSubmitting }) => (
-                    <Form>
-                        <InputField
-                            name="usernameOrEmail"
-                            placeholder="username or email"
-                            label="Username Or Email"
-                        />
-                        <Box mt={4}>
-                            <InputField
-                                name="password"
-                                placeholder="Password"
-                                label="password"
-                                type="password"
-                            />
-                        </Box>
-                        <Flex>
-                            <NextLink href="/forgot-password">
-                                <Link ml="auto">forgot password</Link>
-                            </NextLink>
-                        </Flex>
-                        <Button
-                            mt={4}
-                            type="submit"
-                            isLoading={isSubmitting}
-                            colorScheme="teal"
-                        >
-                            login
-                        </Button>
-                    </Form>
-                )}
-            </Formik>
-        </Wrapper>
-    );
+              login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
 };
 
 export default withUrqlClient(createUrqlClient)(Login);
--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -1,19 +1,19 @@
 import { isServer } from "./isServer";
-import { cacheExchange, Resolver } from "@urql/exchange-graphcache";
+import { Cache, cacheExchange, Resolver } from "@urql/exchange-graphcache";
 import {
-    dedupExchange,
-    Exchange,
-    fetchExchange,
-    stringifyVariables,
+  dedupExchange,
+  Exchange,
+  fetchExchange,
+  stringifyVariables,
 } from "urql";
 import { pipe, tap } from "wonka";
 import {
-    DeletePostMutationVariables,
-    LoginMutation,
-    LogoutMutation,
-    MeDocument,
-    MeQuery,
-    RegisterMutation,
+  DeletePostMutationVariables,
+  LoginMutation,
+  LogoutMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
 } from "./../generated/graphql";
 import { beterUpdateQuery } from "./beterUpdateQuery";
 import Router from "next/router";
@@ -21,239 +21,182 @@ import { VoteMutationVariables } from "../generated/graphql";
 import gql from "graphql-tag";
 
 const errorExchange: Exchange =
-    ({ forward }) =>
-    (ops$) => {
-        return pipe(
-            forward(ops$),
-            tap(({ error }) => {
-                if (error) {
-                    if (error?.message.includes("Not authenticated")) {
-                        Router.replace("/login");
-                    }
-                }
-            })
-        );
-    };
+  ({ forward }) =>
+  (ops$) => {
+    return pipe(
+      forward(ops$),
+      tap(({ error }) => {
+        if (error) {
+          if (error?.message.includes("Not authenticated")) {
+            Router.replace("/login");
+          }
+        }
+      })
+    );
+  };
 
 const cursorPagination = (): Resolver => {
-    return (_parent, fieldArgs, cache, info) => {
-        const { parentKey: entityKey, fieldName } = info;
-        const allFields = cache.inspectFields(entityKey);
-        const fieldInfos = allFields.filter(
-            (info) => info.fieldName === fieldName
-        );
-        const size = fieldInfos.length;
-        if (size === 0) {
-            return undefined;
-        }
+  return (_parent, fieldArgs, cache, info) => {
+    const { parentKey: entityKey, fieldName } = info;
+    const allFields = cache.inspectFields(entityKey);
+    const fieldInfos = allFields.filter((info) => info.fieldName === fieldName);
+    const size = fieldInfos.length;
+    if (size === 0) {
+      return undefined;
+    }
 
-        // Check if the data in the cache or not then add new data requested
-        const fieldKey = `${fieldName}(${stringifyVariables(fieldArgs)})`;
-        const isItInTheCache = cache.resolve(
-            cache.resolve(entityKey, fieldKey) as string,
-            "posts"
-        );
-        info.partial = !isItInTheCache;
+    // Check if the data in the cache or not then add new data requested
+    const fieldKey = `${fieldName}(${stringifyVariables(fieldArgs)})`;
+    const isItInTheCache = cache.resolve(
+      cache.resolve(entityKey, fieldKey) as string,
+      "posts"
+    );
+    info.partial = !isItInTheCache;
 
-        const results: string[] = [];
-        let hasMore = true;
-        fieldInfos.forEach((fi) => {
-            const key = cache.resolve(entityKey, fi.fieldKey) as string;
-            const posts = cache.resolve(key, "posts") as string[];
-            const _hasMore = cache.resolve(key, "hasMore") as boolean;
-            if (!_hasMore) {
-                hasMore = _hasMore;
-            }
-            results.push(...posts);
-        });
+    const results: string[] = [];
+    let hasMore = true;
+    fieldInfos.forEach((fi) => {
+      const key = cache.resolve(entityKey, fi.fieldKey) as string;
+      const posts = cache.resolve(key, "posts") as string[];
+      const _hasMore = cache.resolve(key, "hasMore") as boolean;
+      if (!_hasMore) {
+        hasMore = _hasMore;
+      }
+      results.push(...posts);
+    });
 
-        return {
-            __typename: "PaginatedPosts",
-            posts: results,
-            hasMore: hasMore,
-        };
-
-        // const visited = new Set();
-        // let result: NullArray<string> = [];
-        // let prevOffset: number | null = null;
-
-        // for (let i = 0; i < size; i++) {
-        //     const { fieldKey, arguments: args } = fieldInfos[i];
-        //     if (args === null || !compareArgs(fieldArgs, args)) {
-        //         continue;
-        //     }
-
-        //     const links = cache.resolve(entityKey, fieldKey) as string[];
-        //     const currentOffset = args[offsetArgument];
-
-        //     if (
-        //         links === null ||
-        //         links.length === 0 ||
-        //         typeof currentOffset !== "number"
-        //     ) {
-        //         continue;
-        //     }
-
-        //     const tempResult: NullArray<string> = [];
-
-        //     for (let j = 0; j < links.length; j++) {
-        //         const link = links[j];
-        //         if (visited.has(link)) continue;
-        //         tempResult.push(link);
-        //         visited.add(link);
-        //     }
-
-        //     if (
-        //         (!prevOffset || currentOffset > prevOffset) ===
-        //         (mergeMode === "after")
-        //     ) {
-        //         result = [...result, ...tempResult];
-        //     } else {
-        //         result = [...tempResult, ...result];
-        //     }
-
-        //     prevOffset = currentOffset;
-        // }
-
-        // const hasCurrentPage = cache.resolve(entityKey, fieldName, fieldArgs);
-        // if (hasCurrentPage) {
-        //     return result;
-        // } else if (!(info as any).store.schema) {
-        //     return undefined;
-        // } else {
-        //     info.partial = true;
-        //     return result;
-        // }
+    return {
+      __typename: "PaginatedPosts",
+      posts: results,
+      hasMore: hasMore,
     };
+  };
+};
+
+const invalidateAllPost = (cache: Cache) => {
+  const allFields = cache.inspectFields("Query");
+  const fieldInfos = allFields.filter((info) => info.fieldName === "posts");
+  fieldInfos.forEach((fi) => {
+    cache.invalidate("Query", "posts", fi.arguments || {});
+  });
 };
 
 export const createUrqlClient = (ssrExchange: any, ctx: any) => {
-    let cookie = "";
-    if (isServer()) {
-        cookie = ctx?.req?.headers?.cookie;
-    }
-    return {
-        url: "http://localhost:4000/graphql",
-        fetchOptions: {
-            credentials: "include" as const,
-            headers: cookie
-                ? {
-                      cookie,
-                  }
-                : undefined,
+  let cookie = "";
+  if (isServer()) {
+    cookie = ctx?.req?.headers?.cookie;
+  }
+  return {
+    url: "http://localhost:4000/graphql",
+    fetchOptions: {
+      credentials: "include" as const,
+      headers: cookie
+        ? {
+            cookie,
+          }
+        : undefined,
+    },
+    exchanges: [
+      dedupExchange,
+      cacheExchange({
+        keys: {
+          PaginatedPosts: () => null,
         },
-        exchanges: [
-            dedupExchange,
-            cacheExchange({
-                keys: {
-                    PaginatedPosts: () => null,
-                },
-                resolvers: {
-                    Query: {
-                        posts: cursorPagination(),
-                    },
-                },
-                updates: {
-                    Mutation: {
-                        deletePost: (_result, args, cache, info) => {
-                            cache.invalidate({
-                                __typename: "Post",
-                                id: (args as DeletePostMutationVariables).id,
-                            });
-                        },
-                        vote: (_result, args, cache, info) => {
-                            const { postId, value } =
-                                args as VoteMutationVariables;
-                            const data = cache.readFragment(
-                                gql`
-                                    fragment _ on Post {
-                                        id
-                                        points
-                                        voteStatus
-                                    }
-                                `,
-                                { id: postId } as any
-                            );
+        resolvers: {
+          Query: {
+            posts: cursorPagination(),
+          },
+        },
+        updates: {
+          Mutation: {
+            deletePost: (_result, args, cache, info) => {
+              cache.invalidate({
+                __typename: "Post",
+                id: (args as DeletePostMutationVariables).id,
+              });
+            },
+            vote: (_result, args, cache, info) => {
+              const { postId, value } = args as VoteMutationVariables;
+              const data = cache.readFragment(
+                gql`
+                  fragment _ on Post {
+                    id
+                    points
+                    voteStatus
+                  }
+                `,
+                { id: postId } as any
+              );
 
-                            if (data) {
-                                if (data.voteStatus === value) {
-                                    return;
-                                }
-                                const dataPointsNum = parseInt(data.points);
-                                const voteValue =
-                                    (!data.voteStatus ? 1 : 2) * value;
-                                const newPoints = dataPointsNum + voteValue;
-                                cache.writeFragment(
-                                    gql`
-                                        fragment __ on Post {
-                                            id
-                                            points
-                                            voteStatus
-                                        }
-                                    `,
-                                    {
-                                        id: postId,
-                                        points: newPoints,
-                                        voteStatus: value,
-                                    } as any
-                                );
-                            }
-                        },
-                        createPost: (_result, args, cache, info) => {
-                            const allFields = cache.inspectFields("Query");
-                            const fieldInfos = allFields.filter(
-                                (info) => info.fieldName === "posts"
-                            );
-                            fieldInfos.forEach((fi) => {
-                                cache.invalidate(
-                                    "Query",
-                                    "posts",
-                                    fi.arguments || {}
-                                );
-                            });
-                        },
-                        logout: (_result, args, cache, info) => {
-                            beterUpdateQuery<LogoutMutation, MeQuery>(
-                                cache,
-                                { query: MeDocument },
-                                _result,
-                                () => ({ me: null })
-                            );
-                        },
-                        login: (_result, args, cache, info) => {
-                            beterUpdateQuery<LoginMutation, MeQuery>(
-                                cache,
-                                { query: MeDocument },
-                                _result,
-                                (result, query) => {
-                                    if (result.login.errors) {
-                                        return query;
-                                    } else {
-                                        return { me: result.login.user };
-                                    }
-                                }
-                            );
-                        },
-                        register: (_result, args, cache, info) => {
-                            beterUpdateQuery<RegisterMutation, MeQuery>(
-                                cache,
-                                { query: MeDocument },
-                                _result,
-                                (result, query) => {
-                                    if (result.register.errors) {
-                                        return query;
-                                    } else {
-                                        return { me: result.register.user };
-                                    }
-                                }
-                            );
-                        },
-                    },
-                },
-            }),
-            errorExchange,
-            ssrExchange,
-            fetchExchange,
-        ],
-    };
+              if (data) {
+                if (data.voteStatus === value) {
+                  return;
+                }
+                const dataPointsNum = parseInt(data.points);
+                const voteValue = (!data.voteStatus ? 1 : 2) * value;
+                const newPoints = dataPointsNum + voteValue;
+                cache.writeFragment(
+                  gql`
+                    fragment __ on Post {
+                      id
+                      points
+                      voteStatus
+                    }
+                  `,
+                  {
+                    id: postId,
+                    points: newPoints,
+                    voteStatus: value,
+                  } as any
+                );
+              }
+            },
+            createPost: (_result, args, cache, info) => {
+              invalidateAllPost(cache);
+            },
+            logout: (_result, args, cache, info) => {
+              beterUpdateQuery<LogoutMutation, MeQuery>(
+                cache,
+                { query: MeDocument },
+                _result,
+                () => ({ me: null })
+              );
+            },
+            login: (_result, args, cache, info) => {
+              beterUpdateQuery<LoginMutation, MeQuery>(
+                cache,
+                { query: MeDocument },
+                _result,
+                (result, query) => {
+                  if (result.login.errors) {
+                    return query;
+                  } else {
+                    return { me: result.login.user };
+                  }
+                }
+              );
+              invalidateAllPost(cache);
+            },
+            register: (_result, args, cache, info) => {
+              beterUpdateQuery<RegisterMutation, MeQuery>(
+                cache,
+                { query: MeDocument },
+                _result,
+                (result, query) => {
+                  if (result.register.errors) {
+                    return query;
+                  } else {
+                    return { me: result.register.user };
+                  }
+                }
+              );
+            },
+          },
+        },
+      }),
+      errorExchange,
+      ssrExchange,
+      fetchExchange,
+    ],
+  };
 };
