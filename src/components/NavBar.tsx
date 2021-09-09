cf71dd025c9c30e1ec51311a4e6e31b999ad56f7
Arif Suganda <arifsuganda.su@gmail.com>
2021-09-09 09:33:17 +0800

    reload when logout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,10 +3,12 @@ import React from "react";
 import NextLink from "next/link";
 import { useLogoutMutation, useMeQuery } from "../generated/graphql";
 import { isServer } from "../utils/isServer";
+import { useRouter } from "next/router";
 
 interface NavBarProps {}
 
 const NavBar: React.FC<NavBarProps> = ({}) => {
+    const router = useRouter();
     const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
     const [{ data, fetching }] = useMeQuery({
         pause: isServer(),
@@ -36,8 +38,9 @@ const NavBar: React.FC<NavBarProps> = ({}) => {
                 </NextLink>
                 <Box mr={2}>{data.me.username}</Box>
                 <Button
-                    onClick={() => {
-                        logout();
+                    onClick={async () => {
+                        await logout();
+                        router.reload();
                     }}
                     isLoading={logoutFetching}
                     variant="link"
